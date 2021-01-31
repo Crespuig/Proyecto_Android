ca74c14dfbc334cceb96f0090019400feeb3a7de
Héctor <hectorcrespuig@gmail.com>
2021-01-31 13:02:37 +0100

    mejorada NavigationDrawer con todos los fragments.

--- app/src/main/java/com/example/proyecto_android/activitys/MainActivity.java
+++ app/src/main/java/com/example/proyecto_android/activitys/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.inicioActivity)
+                R.id.nav_home, R.id.nav_lista, R.id.nav_mapa, R.id.nav_favoritos, R.id.nav_clima, R.id.nav_transporte, R.id.nav_estadisticas, R.id.menu_opcion01)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
