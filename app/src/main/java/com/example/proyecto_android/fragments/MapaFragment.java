acdfbeb8c88062745d03b7bfd8fa477e9d44c121
crespuig <hectorcrespuig@gmail.com>
2021-02-10 11:24:04 +0100

    Inicio maps

--- app/src/main/java/com/example/proyecto_android/fragments/MapaFragment.java
+++ app/src/main/java/com/example/proyecto_android/fragments/MapaFragment.java
@@ -1,52 +1,61 @@
 package com.example.proyecto_android.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.proyecto_android.R;
-import com.example.proyecto_android.activitys.LoginActivity;
-import com.example.proyecto_android.activitys.MainActivity;
-import com.example.proyecto_android.activitys.MostrarMonumentoActivity;
 import com.example.proyecto_android.model.MapaViewModel;
-import com.example.proyecto_android.model.Utils;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-public class MapaFragment extends Fragment  {
+public class MapaFragment extends Fragment  implements OnMapReadyCallback {
 
     private MapaViewModel slideshowViewModel;
     MapaFragment context = this;
+    private GoogleMap mMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mapa, container, false);
 
-        Button btnMostrar = (Button) view.findViewById(R.id.btnMapa);
-        btnMostrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MostrarMonumentoActivity.class);
-                startActivity(intent);
-            }
-        });
-
         return view;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.mapa);
+        if (mapFragment != null){
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng palMarquesMalf = new LatLng(725514.652338833780959, 4372961.315235150977969);
+
+        MarkerOptions mo = new MarkerOptions();
+
+        mo.position(palMarquesMalf);
+
+        mo.title("PALACIO DE LOS MARQUESES DE MALFERIT,O, DE LOS CONDES DE BRIZUELA");
+
+        mMap.addMarker(mo);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(palMarquesMalf));
     }
 }
